721e35848250107a911e2ce1ad24126a704e1241
mkruszyk <kruszykm@gmail.com>
2018-03-23 08:01:09 +0100

    done

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '617500923394-hpdor05fis1pdisei0tkp083s7r27187.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'i7y5ameBKAelyy8g3Gh5ghs8',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,39 +1,63 @@
+
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
 const app = express();
-var user = {};
+let googleProfile = {};
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret:config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    (accessToken, refreshToken, profile, cb) => {
+        googleProfile = {
+            id: profile.id,
+            displayName: profile.displayName,
+            email: profile.emails[0].value
+        };
+        cb(null, profile);
+    }
+));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static("styles"));
-app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('index', user);
+  res.render('index', { user: req.user });
+});
+
+app.get('/logged', (req, res) => {
+    res.render('index', { user: googleProfile });
 });
 
 app.get('/styles/index.css', (req, res) => {
   res.sendFile(__dirname + '/styles/index.css');
 });
 
-app.get('/auth/google', (req, res) => {
-  res.render('auth/google');
-});
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+}));
 
 app.get('/logout', (req, res) => {
-  user = {};
-  res.render('index');
-});
-
-app.post('/', (req, res) => {
-  user = req.body;
-  password = req.body.password;
-  if (user.email) user.logged = true;
-  console.log(user);
-  res.render('index', user);
+  req.logout();
+  res.redirect('/');
 });
 
 app.listen(3000);
 app.use((req, res, next) => {
-    res.status(404).send('Error 404!');
+  res.status(404).send('Error 404!');
 });
